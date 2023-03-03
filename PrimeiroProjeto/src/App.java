3fdd7bb62c2b21b714a8e44947690bca659b1c5c
franciscotpezzi <fthunderpezzi@gmail.com>
2023-03-03 13:58:39 -0300

    Initial commit

---
+++ PrimeiroProjeto/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("Oração do Pai Nosso");
+        System.out.println("Pai Nosso que estais nos céus,");
+        System.out.println("Santificado seja o Vosso nome.");
+        System.out.println("Venha a nós o Vosso Reino.");
+        System.out.println("Seja feita a Vossa vontade,");
+        System.out.println("Assim na terra como no Céu.");
+        System.out.println("O pão nosso de cada dia nos dai hoje.");
+        System.out.println("Perdoai as nossas ofensas,");
+        System.out.println("Assim como nós perdoamos a quem nos tem ofendido.");
+        System.out.println("E não nos deixeis cair em tentação,");
+        System.out.println("Mas livrai-nos do mal.");
+        System.out.println("Amém.");
+    }
+}
